e9610c964b725c298f9347daa5ea44641c587580
bao <bao.mokja@gmail.com>
2020-05-24 18:57:10 +0900

    allow x-origin access for project routes

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -6,6 +6,13 @@ const projectRouter = express.Router();
 
 function router(){
 
+    
+    projectRouter.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     projectRouter.get('/addProject', function (req, res) {
         res.render('addProject');
     });
